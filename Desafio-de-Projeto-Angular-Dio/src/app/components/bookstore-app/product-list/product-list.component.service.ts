2bf135e71dde938619498f4456f1b1dbd525af02
Yuri Dazio <dazioy@gmail.com>
2022-07-25 21:27:10 -0300

    projeto finalizado sem api

--- Desafio-de-Projeto-Angular-Dio/src/app/components/bookstore-app/product-list/product-list.component.service.ts
+++ Desafio-de-Projeto-Angular-Dio/src/app/components/bookstore-app/product-list/product-list.component.service.ts
@@ -1,2 +1,19 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/http';
+import {HttpClient, HttpHeaders } from '@angular/common/http';
+import {Book} from "./model/book";
+
+@Injectable()
+
+export class BookService{
+    private url = 'https://localhost:44382/api/bookstore';
+
+    httpOpitions={
+            Headers: new HttpHeaders ({'Content-Type': 'application/json'}),
+    }
+
+    constructor(private http: HttpClient){}
+
+    getBooks(){
+        return this.http.get(this.url)
+    }
+}
